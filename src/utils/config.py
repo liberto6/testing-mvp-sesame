582fe7037a08116755c13df7020f50e2b18660c6
liberto6 <liberto3dejulio@gmail.com>
2026-01-02 19:30:36 +0100

    logica correciones

--- src/utils/config.py
+++ src/utils/config.py
@@ -11,7 +11,7 @@ class Config:
     FORMAT = "int16"
     
     # VAD settings
-    VAD_THRESHOLD = 0.5
+    VAD_THRESHOLD = 0.9
     MIN_SPEECH_DURATION_MS = 400
     MIN_SILENCE_DURATION_MS = 800    # Increased to 800ms for more natural turn-taking and thinking pauses
     
